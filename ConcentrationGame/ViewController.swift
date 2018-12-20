fa209a999f850f7cb2b8781186f5c500cc9ac886
Vitaliy Semenenko <vetaln2o@gmail.com>
2018-12-20 22:13:47 +0200

    Initial Commit

---
+++ ConcentrationGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcentrationGame
+//
+//  Created by Vitalij Semenenko on 12/20/18.
+//  Copyright © 2018 Vitalij Semenenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
