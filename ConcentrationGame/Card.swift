2bf90f773986cae71f7593d15b004a986c097faf
Vitaliy Semenenko <vetaln2o@gmail.com>
2018-12-20 22:23:58 +0200

    Create Card Model

---
+++ ConcentrationGame/Card.swift
@@ -0,0 +1,32 @@
+//
+//  Card.swift
+//  ConcentrationGame
+//
+//  Created by Vitalij Semenenko on 12/20/18.
+//  Copyright © 2018 Vitalij Semenenko. All rights reserved.
+//
+
+import Foundation
+
+class Card: Hashable {
+    
+    var identifier: Int
+    var isFaceUp = false
+    var isMatched = false
+    
+    private static var lastCreatedIdentifier = 1
+    
+    init() {
+        identifier = Card.lastCreatedIdentifier
+        Card.lastCreatedIdentifier += 1
+    }
+    
+    var hashValue: Int {
+        return identifier
+    }
+    
+    static func == (lhs: Card, rhs: Card) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+    
+}
