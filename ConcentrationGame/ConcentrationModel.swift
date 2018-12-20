99be54643f58e7226b63071e674f305b73bc96ec
Vitaliy Semenenko <vetaln2o@gmail.com>
2018-12-20 23:27:32 +0200

    Create Concentration Model, Connect touch cards to model

---
+++ ConcentrationGame/ConcentrationModel.swift
@@ -0,0 +1,30 @@
+//
+//  ConcentrationModel.swift
+//  ConcentrationGame
+//
+//  Created by Vitalij Semenenko on 12/20/18.
+//  Copyright © 2018 Vitalij Semenenko. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    
+    var cardsList: [Card]
+    
+    init(cardsCount: Int) {
+        cardsList = [Card]()
+        let numberOfPairsOfCards = cardsCount / 2
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cardsList += [card,card]
+        }
+        cardsList.shuffle()
+    }
+    
+    func chooseCard(at index: Int) {
+        cardsList[index].isFaceUp = !cardsList[index].isFaceUp
+        print("Card with identifier: \(cardsList[index].identifier) isFaseUp: \(cardsList[index].isFaceUp)")
+    }
+    
+}
--- ConcentrationGame/ConcentrationViewController.swift
+++ ConcentrationGame/ConcentrationViewController.swift
@@ -11,14 +11,29 @@ import UIKit
 class ConcentrationViewController: UIViewController {
     
     @IBOutlet var cardButtons: [UIButton]!
-    
     @IBOutlet weak var flipCountLabel: UILabel!
+    
+    lazy var game = Concentration(cardsCount: cardButtons.count)
+    var flipCounts = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCounts)"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardIndex = cardButtons.firstIndex(of: sender) {
+            game.chooseCard(at: cardIndex)
+            flipCounts += 1
+        } else {
+            print("Card are not in Buttons List!")
+        }
+    }
+    
 
 }
 
